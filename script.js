c43489958a729061550a72deacead7286e082015
gabrielribeiro <gabrielribeirotnc@gmail.com>
2024-10-02 22:32:59 +0100

    fix: fix count with invalid step and settings in display

--- script.js
+++ script.js
@@ -5,9 +5,30 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO]Faltam dados!')
-
+        window.alert('[ERRO] Faltam dados!')
     } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
+        // Validação do valor do passo
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `\u{1f449} ${c} `
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `\u{1f449} ${c} `
+            }
+        }
+        res.innerHTML += `\u{1F3c1}` // Bandeira de chegada
     }
 }
